b2e5cfc797abc2b04ee8619c00b8efc787f753fe
suyou <suyoutain41@gmail.com>
2022-09-12 10:48:20 +0800

    add header

--- products.py
+++ products.py
@@ -8,3 +8,7 @@ while True:
 	products.append([name, price])
 for a in products:
 	print(a[0], a[1])
+with open("products.csv", "w" , encoding="utf-8") as g:
+	g.write("商品,價格\n")
+	for p in products:
+		g.write(p[0] + "," + p[1] + "\n")
